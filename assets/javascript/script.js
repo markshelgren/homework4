e62857a670f9799f296853b843e1752971d5708c
markshelgren <67986616+markshelgren@users.noreply.github.com>
2020-10-07 19:41:21 -0700

    Work in Progress needs to be saved

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,14 @@
 var curQuestion = 0;
 var timeoutHandle;
 var answerId = "";
-var userAnswer = "";
+var userAnswer = 0;
+
+// For keeping score
+var totCorrect = 0;
+var totWrong = 0;
+var totQuestions = 5;
+var totScore = "";
+var questionCount = "";
 
 function countdown(minutes, seconds) {
 	var seconds = 60;
@@ -30,24 +37,37 @@ function countdown(minutes, seconds) {
 	tick();
 }
 
+// Suppress the seed html before the first question
+$("#question").empty();
+$(".text-danger").empty();
+$("button").empty();
+$(".question bg-white p-3 border-bottom").empty();
+$("#A0").empty();
+$("#A1").empty();
+$("#A2").empty();
+$("#A3").empty();
+$("#A4").empty();
+
 // Start the timer and remove the start button when Start is pressed
 $("#start").on("click", function () {
 	countdown(5);
 	// remove the start button when clicked
 	$("#start").remove();
-	// game.loadQuestion();
-	// Loop through the questions
 
-	for (let i = 0; i < questions.length; i++) {
-		const element = questions[i];
+	// Display the first question
+	curQuestion = 0;
 
-		console.log(i);
-		displayQuestions();
-	}
+	questionCount =
+		"Question (" + (curQuestion + 1) + " of " + totQuestions + ")";
+	console.log("Question (" + (curQuestion + 1) + " of " + totQuestions + ")");
+	console.log(questionCount);
+	console.log(questions[curQuestion].question);
+	$("#questionStatus").html(questionCount);
+
+	displayQuestions();
 });
 
 function displayQuestions() {
-	console.log(questions[curQuestion].question);
 	$("#question").html(questions[curQuestion].question);
 	loadAnswers();
 }
@@ -58,13 +78,7 @@ function loadAnswers() {
 		const element = questions[curQuestion].answers[j];
 
 		answerId = "#A" + j;
-
-		console.log(answerId);
-		console.log("this is the Current question " + curQuestion);
-
-		// 4. Then give each "letterBtn" a data-attribute called "data-letter".
-		console.log(answerId);
-
+		console.log(questions[curQuestion].answers[j]);
 		$(answerId).html(questions[curQuestion].answers[j]);
 	}
 }
@@ -73,153 +87,114 @@ function loadAnswers() {
 
 $("#A0").on("click", function () {
 	// Inside the on-click event...
-	userAnswer = "0";
+	userAnswer = 0;
+	console.log("the users answer is " + userAnswer);
+	checkAnswer();
+});
+
+$("#A1").on("click", function () {
+	// Inside the on-click event...
+	userAnswer = 1;
+	console.log("the users answer is " + userAnswer);
+	checkAnswer();
+});
+
+$("#A2").on("click", function () {
+	// Inside the on-click event...
+	userAnswer = 2;
+	console.log("the users answer is " + userAnswer);
+	checkAnswer();
+});
+
+$("#A3").on("click", function () {
+	// Inside the on-click event...
+	userAnswer = 3;
 	console.log("the users answer is " + userAnswer);
 	checkAnswer();
 });
 
 // Function to check the user answer against the correct answer.
 
-function checkAnswer() {}
+function checkAnswer() {
+	console.log(questions[curQuestion].answers[userAnswer]);
+	console.log(questions[curQuestion].correctAnswer);
+	if (
+		questions[curQuestion].answers[userAnswer] ==
+		questions[curQuestion].correctAnswer
+	) {
+		console.log("The answer is correct for question " + curQuestion);
+		console.log(
+			"The value of the answer is  " + questions[curQuestion].correctAnswer
+		);
 
-// click event when you click the answer
+		//Add to total correct
+		totCorrect++;
 
-$(document).on("click", ".answer-button", function (e) {
-	game.clicked(e);
-});
+		// Update the scoreboard on the screen
 
-$(document).on("click", "#reset", function () {
-	game.reset();
-});
+		curQuestion = curQuestion + 1;
+
+		if (curQuestion < 5) {
+			questionCount =
+				"Question (" + (curQuestion + 1) + " of " + totQuestions + ")";
+			console.log(
+				"Question (" + (curQuestion + 1) + " of " + totQuestions + ")"
+			);
+			console.log(questionCount);
+			console.log(questions[curQuestion].question);
+			$("#questionStatus").html(questionCount);
+			displayQuestions();
+		}
+	} else {
+		console.log("The answer is wrong for question " + curQuestion);
+		console.log(
+			"The answer entered by the user is  " +
+				questions[curQuestion].answers[userAnswer]
+		);
+		console.log(
+			"The correct answer is  " + questions[curQuestion].correctAnswer
+		);
+		//Add to total incorrect
+		totWrong++;
+
+		curQuestion = curQuestion + 1;
+		if (curQuestion < 5) {
+			displayQuestions();
+		}
+	}
+}
 
 // Variable for questions, an array of objects
 
 var questions = [
 	{
-		question: "What is the state tree of North Carolina?",
-		answers: ["Dogwood", "Longleaf Pine", "Bald Cypress", "Fraser Fir"],
-		correctAnswer: "Dogwood",
-	},
-	/* 	{
-		question: "North Carolina grows the most what in the nation?",
-		answers: ["Tomatoes", "Sweet Potatoes", "Broccoli", "Soybeans"],
-		correctAnswer: "Sweet Potatoes",
+		question: "How do you round the number 7.25, to the nearest integer?",
+		answers: ["round(7.25)", "rnd(7.25)", "Math.round(7.25)", "Math.rnd(7.25)"],
+		correctAnswer: "Math.round(7.25)",
 	},
 	{
-		question: "What is the state bird?",
-		answers: ["Bald Eagle", "Blue Jay", "Cardinal", "Wild Turkey"],
-		correctAnswer: "Cardinal",
-	}, */
+		question: "Which event occurs when the user clicks on an HTML element?",
+		answers: ["onchange", "onclick", "onmouseclick", "onmouseover"],
+		correctAnswer: "onclick",
+	},
+	{
+		question: "Which operator is used to assign a value to a variable?",
+		answers: ["=", "X", "*", "-"],
+		correctAnswer: "-",
+	},
+	{
+		question: "How to write an IF statement in JavaScript?",
+		answers: ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"],
+		correctAnswer: "if (i == 5)",
+	},
+	{
+		question: "How does a FOR loop start?",
+		answers: [
+			"for (i = 0; i <= 5)",
+			"for (i <= 5; i++)",
+			"for i = 1 to 5",
+			"for (i = 0; i <= 5; i++)",
+		],
+		correctAnswer: "for (i = 0; i <= 5; i++)",
+	},
 ];
-
-var game = {
-	questions: questions,
-	currentQuestion: 0,
-	counter: 30,
-	correct: 0,
-	incorrect: 0,
-	unanswered: 0,
-
-	countdown: function () {
-		game.counter--;
-		$("#counter").html(game.counter);
-		if (game.counter <= 0) {
-			console.log("TIME UP!");
-			game.timeUp();
-		}
-	},
-	loadQuestion: function () {
-		timer = setInterval(game.countdown, 1000);
-		$("#subwrapper").html(
-			"<h2> Time to Guess: <span id ='counter'>30</span> Seconds</h2>"
-		);
-		$("#subwrapper").append(
-			"<h2>" + questions[game.currentQuestion].question + "</h2>"
-		);
-		for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
-			$("#subwrapper").append(
-				'<button class="answer-button id="button- ' +
-					i +
-					'" data-name="' +
-					questions[game.currentQuestion].answers[i] +
-					'">' +
-					questions[game.currentQuestion].answers[i] +
-					"</button>"
-			);
-		}
-	},
-	nextQuestion: function () {
-		game.counter = 30;
-		$("#counter").html(game.counter);
-		game.currentQuestion++;
-		game.loadQuestion();
-	},
-	timeUp: function () {
-		clearInterval(timer);
-		game.unanswered++;
-		$("#subwrapper").html("<h2>Out of time!<h2>");
-		$("#subwrapper").append(
-			"<h3>The correct answer was: " +
-				questions[game.currentQuestion].correctAnswer +
-				"</h3>"
-		);
-		if (game.currentQuestion == questions.length - 1) {
-			setTimeout(game.results, 3 * 1000);
-		} else {
-			setTimeout(game.nextQuestion, 3 * 1000);
-		}
-	},
-	results: function () {
-		clearInterval(timer);
-		$("#subwrapper").html("<h2>Complete!</h2>");
-		$("#subwrapper").append(" Correct: " + game.correct + "<br/>");
-		$("#subwrapper").append(" Incorrect: " + game.incorrect + "<br/>");
-		$("#subwrapper").append(" Unanswered: " + game.unanswered + "<br/>");
-		$("#subwrapper").append("<button id= reset>Try again?</button>");
-	},
-	clicked: function (e) {
-		clearInterval(timer);
-		if (
-			$(e.target).data("name") == questions[game.currentQuestion].correctAnswer
-		) {
-			game.answeredCorrectly();
-		} else {
-			game.answeredIncorrectly();
-		}
-	},
-	answeredCorrectly: function () {
-		console.log("right!");
-		clearInterval(timer);
-		game.correct++;
-		$("#subwrapper").html("<h2> CORRECT!</h2>");
-		if (game.currentQuestion == questions.length - 1) {
-			setTimeout(game.results, 2 * 1000);
-		} else {
-			setTimeout(game.nextQuestion, 2 * 1000);
-		}
-	},
-	answeredIncorrectly: function () {
-		console.log("wrong");
-		clearInterval(timer);
-		game.incorrect++;
-		$("#subwrapper").html("<h2> Wrong!</h2>");
-		$("#subwrapper").append(
-			"<h3>The correct answer was: " +
-				questions[game.currentQuestion].correctAnswer +
-				"</h3>"
-		);
-		if (game.currentQuestion == questions.length - 1) {
-			setTimeout(game.results, 2 * 1000);
-		} else {
-			setTimeout(game.nextQuestion, 2 * 1000);
-		}
-	},
-	reset: function () {
-		game.currentQuestion = 0;
-		game.counter = 0;
-		game.correct = 0;
-		game.incorrect = 0;
-		game.unanswered = 0;
-		game.loadQuestion();
-	},
-};
